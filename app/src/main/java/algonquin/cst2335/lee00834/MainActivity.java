09a9e00e7e6d7a128b819b005c93e1850ee30106
Wan-Hsuan Lee <lee00834@algonquinlive.com>
2023-05-24 21:17:10 -0400

    Week2 Completed ViewModel class (4/6)
    Signed-off-by: Wan-Hsuan Lee <lee00834@algonquinlive.com>

--- app/src/main/java/algonquin/cst2335/lee00834/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/lee00834/MainActivity.java
@@ -1,6 +1,8 @@
 package algonquin.cst2335.lee00834;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 import android.view.View;
@@ -8,24 +10,29 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import algonquin.cst2335.lee00834.MainViewModel;
 import algonquin.cst2335.lee00834.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding variableBinding;
+    private MainViewModel model;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        model = new ViewModelProvider(this).get(MainViewModel.class);
+
         variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(variableBinding.getRoot()); //loads XML on screen
 
-        TextView mytext = variableBinding.theText;
-        Button mybutton = variableBinding.theButton;
-        EditText myedit = variableBinding.theEdit;
-        mybutton.setOnClickListener( view -> {
-                String editString = myedit.getText().toString();
-                mytext.setText("Your edit text has: " + editString);
+        variableBinding.theButton.setOnClickListener( view -> {
+            model.editString.postValue(variableBinding.theEdit.getText().toString());
+            variableBinding.theText.setText(model.editString.getValue());
+        });
+
+        model.editString.observe(this, s -> {
+            variableBinding.theText.setText(model.editString.getValue());
         });
 
 //        setContentView(R.layout.activity_main);
---
+++ app/src/main/java/algonquin/cst2335/lee00834/MainViewModel.java
@@ -0,0 +1,8 @@
+package algonquin.cst2335.lee00834;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    public MutableLiveData<String> editString = new MutableLiveData<>();
+}
