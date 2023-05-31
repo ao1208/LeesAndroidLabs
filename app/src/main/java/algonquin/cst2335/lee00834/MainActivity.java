1378e35e70d21142350dfb690e9de3db447c417e
Wan-Hsuan Lee <lee00834@algonquinlive.com>
2023-05-31 01:52:05 -0400

    Finished phone call of Lab 4
    Signed-off-by: Wan-Hsuan Lee <lee00834@algonquinlive.com>

--- app/src/main/java/algonquin/cst2335/lee00834/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/lee00834/MainActivity.java
@@ -2,13 +2,69 @@ package algonquin.cst2335.lee00834;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import algonquin.cst2335.lee00834.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private String TAG = "MainActivity";
+    protected ActivityMainBinding binding;
+
+    //equivalent to        static void main(String args[])
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        super.onCreate(savedInstanceState); //calling onCreate from parent class
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        //loads an XML file on the page
+        setContentView(binding.getRoot());
+        Log.e(TAG, "In onCreate()");
+
+        binding.loginButton.setOnClickListener( click -> {
+            Log.e(TAG, "You clicked the button");
+
+            //where to go:                   leaving here    going to SecondActivity
+            Intent nextPage = new Intent( this, SecondActivity.class);
+
+            nextPage.putExtra("Email", binding.emailText.getText().toString());
+            nextPage.putExtra("Password", binding.passwdText.getText().toString());
+
+            //go to another page
+            startActivity(nextPage);
+        });
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(TAG, "In onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG, "In onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(TAG, "In onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(TAG, "In onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(TAG, "In onDestroy()");
     }
 }
---
+++ app/src/main/java/algonquin/cst2335/lee00834/SecondActivity.java
@@ -0,0 +1,66 @@
+package algonquin.cst2335.lee00834;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import algonquin.cst2335.lee00834.databinding.ActivitySecondBinding;
+
+public class SecondActivity extends AppCompatActivity {
+
+    ActivitySecondBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding  = ActivitySecondBinding.inflate(getLayoutInflater());
+        //loads the XML file  /res/layout/activity_second.xml
+        setContentView(binding.getRoot());
+
+        Intent fromPrevious = getIntent();//return the Intent that got us here
+//should be variables in fromPrevious:
+
+        //null if EMAIL is not found
+        String email = fromPrevious.getStringExtra("Email");//get the value of EMAIL variable
+        String passwd = fromPrevious.getStringExtra("Password");
+
+        binding.textView3.setText("Welcome back " + email);
+
+        binding.callButton.setOnClickListener( click -> {
+            String phoneNumber = binding.editTextPhone.getText().toString();
+            Intent call = new Intent(Intent.ACTION_DIAL);
+            call.setData(Uri.parse("tel:" + phoneNumber ));
+            startActivity(call);
+        });
+
+//        binding.changeButton.setOnClickListener( click -> {
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            ActivityResultLauncher<Intent> cameraResult = registerForActivityResult(
+                    new ActivityResultContracts.StartActivityForResult(),
+                    new ActivityResultCallback<ActivityResult>(){
+                        @Override
+                        public void onActivityResult(ActivityResult result) {
+                            if (result.getResultCode() == Activity.RESULT_OK) {
+                                Intent data = result.getData();
+                                Bitmap thumbnail = data.getParcelableExtra("data");
+                                ImageView profileImage = null;
+                                profileImage.setImageBitmap (thumbnail);
+                            }
+                        }
+                    });
+            cameraResult.launch(cameraIntent);
+//        });
+
+    }
+}
